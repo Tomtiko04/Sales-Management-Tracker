ccf004e973ed63e2775ff0bf3cce59d3426dbc05
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2024-06-12 08:56:00 +0100

    Setting up environment

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://twuylquryexsgasuhofc.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3dXlscXVyeWV4c2dhc3Vob2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2NzQzODAsImV4cCI6MjAzMzI1MDM4MH0.j-7vI_Y2vx-odwpW7fwMdF466En9KV19JBb606wY9Ng";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
