138ac029e3c1779d5cdcb3dfbafe70862f2b61a4
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2024-10-28 22:17:45 +0100

    Pagination for Category

--- src/features/producer/category/useGetProductCategory.js
+++ src/features/producer/category/useGetProductCategory.js
@@ -1,16 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductCategory as getProductCategoryApi } from "../../../services/apiCategoryManagement";
 
-export default function useGetProductCategory() {
+export default function useGetProductCategory(page = 1, pageSize = 10, searchTerm = "") {
+	const startRange = (page - 1) * pageSize;
+	const endRange = startRange + pageSize - 1;
+
 	const {
-		data: getCategories,
-		isPending: isGettingCategories,
+		data,
+		isLoading: isGettingCategories,
 		error,
 	} = useQuery({
-		queryKey: ["product-category"],
-		queryFn: getProductCategoryApi,
+		queryKey: ["product-category", page, pageSize, searchTerm],
+		queryFn: () => getProductCategoryApi(startRange, endRange, searchTerm),
+		keepPreviousData: true,
 	});
 
+	const getCategories = data?.categories || [];
+	const totalCategories = data?.totalCount || 0;
+
 	if (error) throw new Error(error.message);
-	return { getCategories, isGettingCategories };
+	return { getCategories, totalCategories, isGettingCategories };
 }
--- src/services/apiCategoryManagement.js
+++ src/services/apiCategoryManagement.js
@@ -11,14 +11,25 @@ export async function addProductCategory({ categoryName, userId, companyName }) 
 	return data;
 }
 
-export async function getProductCategory() {
+export async function getProductCategory(startRange, endRange, searchTerm = "") {
 	const id = Cookies.get("userId");
-	let { data: productCategory, error } = await supabase.from("productCategory").select("*").eq("user_id", id);
+	let query = supabase
+		.from("productCategory")
+		.select("*", { count: "exact" })
+		.eq("user_id", id)
+		.range(startRange, endRange);
+
+	if (searchTerm) {
+		query = query.ilike("categoryName", `%${searchTerm}%`);
+	}
+
+	const { data: productCategory, error, count } = await query;
 
 	if (error) throw new Error(error.message);
 
-	return productCategory;
+	return { categories: productCategory, totalCount: count };
 }
+
 
 export async function deleteProductCategory(categoryid) {
 	const { error } = await supabase.from("productCategory").delete().eq("id", categoryid);
