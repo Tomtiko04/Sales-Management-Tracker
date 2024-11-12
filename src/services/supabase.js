6c2959cf0abdd263b1a83136e99d5578c4e3e8d6
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2024-11-12 07:53:11 +0100

    Environment key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = "https://xeiymqpwkikgxtzdaqju.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlaXltcXB3a2lrZ3h0emRhcWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1NzcyNjQsImV4cCI6MjA0MDE1MzI2NH0.JT0tzjRLB0OatW8B6a1-Ap_Be1FYn1BcrV20pUZeuas";
+export const supabaseUrl = process.env.SALES_MANAGEMENT_APP_SUPABASE_URL;
+const supabaseKey = process.env.SALES_MANAGEMENT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
