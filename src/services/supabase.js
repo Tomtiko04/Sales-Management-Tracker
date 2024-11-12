7aac766694ba82732102b071eccb8248f4edb7c9
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2024-11-12 08:28:53 +0100

    Key protection

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = process.env.SALES_MANAGEMENT_APP_SUPABASE_URL;
-const supabaseKey = process.env.SALES_MANAGEMENT_APP_SUPABASE_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
